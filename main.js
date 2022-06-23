c3f8fd2377dc14638997db9250c17ca4058975d7
Ehobee <ritsu@abv.bg>
2022-06-23 13:27:58 +0300

    Create and render player initial form

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
+//create class of Player
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        };
+        this.width = 100;
+        this.heigth = 100;
+    };
+    drawPlayer() {
+        context.fillRect(this.position.x, this.position.y, this.width, this.heigth)
+    };
+};
+
+//draw new player by creating an object and call the drawPlayer method
+const player = new Player();
+player.drawPlayer()
+
 //console.log(context)
